3a066ca7d885f4eea5180d517b4d7f5097495d10
Trevor Cooper <tcooper@sdsc.edu>
2013-11-21 11:46:25 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/paraview/version.mk
+++ src/paraview/version.mk
@@ -1,3 +1,14 @@
-NAME    = paraview
-VERSION = 3.14.1
-RELEASE = 1
+NAME               = paraview
+VERSION            = 3.14.1
+RELEASE            = 1
+PKGROOT            = /opt/paraview
+
+SRC_SUBDIR         = paraview
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
